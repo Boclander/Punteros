37a467f4c64af448cfc117afa4a1cfc0d8131848
Boclander <Boclander@gmail.com>
2018-10-22 15:43:57 -0300

    Punteros ejercicio realloc

--- Punteros struct/main.c
+++ Punteros struct/main.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAMANIOARRAY 5
 typedef struct
 {
     int a;
     char b;
-}eDato;
+} eDato;
 
 int main()
 {
     int* pEntero;
     int i;
 
-    pEntero = (int*) malloc(sizeof(int)*TAMANIOARRAY); //Tamaño de array.
-    if(pEntero != NULL){
-        for(i=0; i<TAMANIOARRAY; i++)
+    pEntero = (int*) malloc(sizeof(int)*5); //Tamaño de array.
+    if(pEntero != NULL)
+    {
+        for(i=0; i<5; i++)
         {
             printf("Ingrese un numero: ");
             scanf("%d", pEntero+i);
         }
 
-        for(i=0; i<TAMANIOARRAY; i++)
+        for(i=0; i<5; i++)
         {
             printf("%d - ", *(pEntero+i));
         }
+    }
+    system("pause");
+    system("cls");
+    printf("Ingrese otros 5 numeros\n\n");
+
+    pEntero = realloc(pEntero, sizeof(int)*10);
+    if(pEntero != NULL)
+    {
+        for(i=5; i<10; i++)
+        {
+            printf("Ingrese un numero: ");
+            scanf("%d", pEntero+i);
         }
+
+        for(i=0; i<10; i++)
+        {
+            printf("%d - ", *(pEntero+i));
+        }
+    }
 }
